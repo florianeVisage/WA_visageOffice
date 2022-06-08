f379628e084ced6d824ac91cebd76af137443b6f
florianeVisage <floriane@visage.jobs>
2022-06-08 19:15:53 +0200

    feat: improvments on rooms and specific zones

--- src/index.ts
+++ src/index.ts
@@ -4,21 +4,24 @@ import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 console.log('Script started successfully');
 
 let currentPopup: any = undefined;
-const deskNames: string[] = ['Nebulae', 'Equinox', 'Pulsar', 'Lionel']
+const deskNames: string[] = ['Nebulae', 'Equinox', 'Pulsar', 'Constellation', 'Lionel', 'Chief_Architect']
 
 // Waiting for the API to be ready
 WA.onInit().then(() => {
     console.log('Scripting API ready');
     // console.log('Player tags: ',WA.player.tags)
-
     
     for (const name of deskNames) {
         WA.room.onEnterLayer(name + 'OfficeTrigger').subscribe(() => {
             currentPopup = WA.ui.openPopup(name+'Title', `Welcome to ${name}'s office`, [])
         })
-
         WA.room.onLeaveLayer(name + 'OfficeTrigger').subscribe(closePopUp)
     }
+
+    WA.room.onEnterLayer('WarRoomOfficeTrigger').subscribe(() => {
+        currentPopup = WA.ui.openPopup('WarRoomTitle', 'War Room', [])
+    })
+    WA.room.onLeaveLayer('WarRoomOfficeTrigger').subscribe(closePopUp)
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
