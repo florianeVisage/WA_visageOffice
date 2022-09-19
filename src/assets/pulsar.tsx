bfcee7297d8795967533704715fb931643e635c7
Vicente Noriega <vicente@visage.jobs>
2022-09-19 09:47:57 +0200

    pulsar medieval office v1 (untested)

---
+++ src/assets/pulsar.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="pulsar" tilewidth="32" tileheight="32" tilecount="14" columns="7">
+ <image source="../../../../pulsar-medieval.png" width="225" height="75"/>
+</tileset>
