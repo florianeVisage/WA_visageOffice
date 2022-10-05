9903f2633bf1f15d217ed183fb1705557d5746b3
Vicente Noriega <vicente@visage.jobs>
2022-10-05 17:42:51 +0200

    fix pulsar map & last collision & silent zone changes

--- src/assets/pulsar-medieval.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="pulsar-medieval" tilewidth="32" tileheight="32" tilecount="10" columns="5">
- <image source="pulsar-medieval.png" width="168" height="70"/>
-</tileset>
